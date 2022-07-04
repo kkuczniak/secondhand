fa3ad5f0a832f10ed30356f7ad4dd6019243fbcb
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-04 20:55:43 +0200

    added Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { View, Text, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { Divider } from 'react-native-elements';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+} from 'react-native';
 import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
+import RestaurantItems, {
+  localRestaurants,
+} from '../components/RestaurantItems';
+import BottomTabs from '../components/BottomTabs';
+
+const YELP_API_KEY =
+  'vx79EbX9jNWafyAtAA3TX5hEBunJkl8PhzKALmRlWugUAA0N0fhnMtzaimI_NWITwhlxixLWJp0wpnXD1MJRx22bkLHsTGW0vWkxPbqhqfePRe20cW9M0qF9VP6LYXYx';
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState('San Francisco');
+  const [activeTab, setActiveTab] = useState('Delivery');
+
+  const getRestaurantFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
+
+  useEffect(() => {
+    getRestaurantFromYelp();
+  }, [city, activeTab]);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ backgroundColor: '#fff', padding: 15 }}>
-        <HeaderTabs />
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <SearchBar cityHandler={setCity} />
       </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
+      <Divider width={1} />
+      <BottomTabs />
     </SafeAreaView>
   );
 }
