657883da70d12cb61978a94c52c592efd10c6f0d
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-07-04 20:56:23 +0200

    added tabs

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,77 +1,23 @@
-// import React, { useState } from 'react';
-// import { View, Text, TouchableOpacity } from 'react-native';
-
-// export default function HeaderTabs(props) {
-//   const { activeTab, setActiveTab } = useState('Delivery');
-//   return (
-//     <View
-//       style={{
-//         flexDirection: 'row',
-//         alignSelf: 'center',
-//       }}
-//     >
-//       <HeaderButton
-//         text='Delivery'
-//         btnColor='black'
-//         textColor='white'
-//         activeTab={activeTab}
-//         setActiveTab={setActiveTab}
-//       />
-//       <HeaderButton
-//         text='Pickup'
-//         btnColor='white'
-//         textColor='black'
-//         activeTab={activeTab}
-//         setActiveTab={setActiveTab}
-//       />
-//     </View>
-//   );
-// }
-
-// const HeaderButton = (props) => (
-//   <TouchableOpacity
-//     style={{
-//       backgroundColor: props.activeTab == props.text ? 'black' : 'white',
-//       paddingVertical: 6,
-//       paddingHorizontal: 16,
-//       borderRadius: 30,
-//     }}
-//     onPress={() => props.setActiveTab(props.text)}
-//   >
-//     <Text
-//       style={{
-//         color: props.activeTab == props.text ? 'white' : 'black',
-//         color: 'black',
-//         fontSize: 15,
-//         fontWeight: '900',
-//       }}
-//     >
-//       {props.text}
-//     </Text>
-//   </TouchableOpacity>
-// );
-
 import React from 'react';
 import { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function HeaderTabs(props) {
-  const [activeTab, setActiveTab] = useState('Delivery');
   return (
     <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
       <HeaderButton
         text='Delivery'
         bgColor='black'
         textColor='white'
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
+        activeTab={props.activeTab}
+        setActiveTab={props.setActiveTab}
       />
       <HeaderButton
-        text='Pick up'
+        text='Pickup'
         bgColor='white'
         textColor='black'
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
+        activeTab={props.activeTab}
+        setActiveTab={props.setActiveTab}
       />
     </View>
   );
@@ -91,7 +37,7 @@ const HeaderButton = (props) => (
       style={{
         color: props.activeTab == props.text ? 'white' : 'black',
         fontSize: 15,
-        fontWeight: '900',
+        fontWeight: 'bold',
       }}
     >
       {props.text}
---
+++ components/SearchBar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+export default function SearchBar({ cityHandler }) {
+  return (
+    <View style={{ marginTop: 15, flexDirection: 'row' }}>
+      <GooglePlacesAutocomplete
+        query={{ key: 'AIzaSyBOmIVW9OqEo3H9Ff68uRZRKRb0Y4bK9xE' }}
+        onPress={(data, details = null) => {
+          console.log(data.description);
+          const city = data.description.split(',')[0];
+          cityHandler(city);
+        }}
+        placeholder='Search'
+        styles={{
+          textInputContainer: {
+            backgroundColor: '#eee',
+            borderRadius: 50,
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginRight: 10,
+          },
+          textInput: {
+            backgroundColor: '#eee',
+            height: 38,
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginTop: 7,
+            borderRadius: 20,
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+        }}
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name='location-sharp' size={24} />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              flexDirection: 'row',
+              marginRight: 8,
+              backgroundColor: 'white',
+              padding: 9,
+              borderRadius: 30,
+              alignItems: 'center',
+            }}
+          >
+            <AntDesign
+              name='clockcircle'
+              size={13}
+              style={{ marginRight: 6 }}
+            />
+            <Text>Search</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
